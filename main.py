262a976de88b3975b9288db1cb150581b7bb693c
Roshan Dhanashekeran <85237206+roshandhanasekaran@users.noreply.github.com>
2022-01-18 20:06:57 +0000

    nothing

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ pet_name=input("Whats your pet name\n")
 print(city_name + pet_name)
 print("your band name is" + " " + city_name + pet_name)
 
-
+print("hello Everyone") 
+city_name=input("Which city did you grow up\n")
+pet_name=input("Whats your pet name\n")
+print(city_name + pet_name)
+print("your band name is" + " " + city_name + pet_name)
